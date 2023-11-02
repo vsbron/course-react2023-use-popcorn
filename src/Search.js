844d7bdbe5c1117c252ab56a75c08a9e2e453943
vsbron <vsbron@gmail.com>
2023-11-02 13:42:31 +0300

    Search field has the focus on the component mount

--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,16 @@
+import { useEffect, useRef } from "react";
+
 // Search component that displays the searchbar
 export default function Search({ query, setQuery }) {
+
+  // Creating a reference for input field using useRef hook
+  const inputEl = useRef(null);
+
+  // Putting focus on the search field on component mount
+  useEffect(() => {
+    inputEl.current.focus();
+  })
+
   // Returns controlled input element
   return (
     <input
@@ -8,6 +19,7 @@ export default function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl} // Putting the ref to the inputEl variable
     />
   );
 }
