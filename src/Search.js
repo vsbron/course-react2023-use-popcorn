08ef9d4d0a2b8215ef974199a09ac09f48698123
vsbron <vsbron@gmail.com>
2023-11-02 13:51:19 +0300

    Canceled the onload focus, focus goes to search field when Enter key is pressed

--- src/Search.js
+++ src/Search.js
@@ -2,14 +2,29 @@ import { useEffect, useRef } from "react";
 
 // Search component that displays the searchbar
 export default function Search({ query, setQuery }) {
-
   // Creating a reference for input field using useRef hook
   const inputEl = useRef(null);
 
   // Putting focus on the search field on component mount
   useEffect(() => {
-    inputEl.current.focus();
-  })
+    // Creating callback function
+    function callback(e) {
+      // if current focus is on search field - do nothing
+      if (document.activeElement === inputEl.current) return;
+
+      // If pressed key is Enter, then put the focus and reset the query
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+
+    // Add the event listener
+    document.addEventListener("keydown", callback);
+
+    // Remove the eventlistener when component unmounts
+    return () => document.removeEventListener("keydown", callback);
+  }, [setQuery]);
 
   // Returns controlled input element
   return (
